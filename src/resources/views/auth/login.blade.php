d0d1dd2df33d2633b1a683ae3b4387353d602855
daiki hayakawa <moko021917@outlook.jp>
2025-08-16 14:44:02 +0900

    ログイン画面作成 #7

---
+++ src/resources/views/auth/login.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+
+@section('css')
+    <link rel="stylesheet" href="{{ asset('css/auth/login.css') }}">
+@endsection
+
+@section('content')
+    <h2 class="form__title">Login</h2>
+    <form class="login__form" action="/login" method="POST">
+        <div class="form__group">
+            <label>
+                <img src="{{ asset('images/email.png') }}" alt="メールアイコン">
+            </label>
+            <input type="text" name="email" placeholder="Email">
+        </div>
+        <div class="form__group">
+            <label>
+                <img src="{{ asset('images/lock.png') }}">
+                <input type="text" name="password" placeholder="Password">
+            </label>
+        </div>
+        <div class="form__button">
+            <button class="form__button--submit">ログイン</button>
+        </div>
+    </form>
+@endsection
