6c66dd00c3e1a90d72aeba755f34b65e72ea0b22
daiki hayakawa <moko021917@outlook.jp>
2025-10-28 19:21:37 +0900

    ER図修正#71

---
+++ laravel_backup.sql
@@ -0,0 +1,3 @@
+mysqldump: [Warning] World-writable config file '/etc/mysql/conf.d/my.cnf' is ignored.
+Enter password: 
+mysqldump: Got error: 1045: Access denied for user 'root'@'localhost' (using password: YES) when trying to connect
