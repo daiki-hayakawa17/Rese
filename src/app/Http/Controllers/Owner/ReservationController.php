168789fbd7fe1b52f75ad159258755b4ffd61914
daiki hayakawa <moko021917@outlook.jp>
2025-11-05 19:53:13 +0900

    最終確認・修正#80

--- src/app/Http/Controllers/Owner/ReservationController.php
+++ src/app/Http/Controllers/Owner/ReservationController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Owner;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Carbon\Carbon;
 use App\Models\Reservation;
 use App\Models\Shop;
 
@@ -12,16 +11,14 @@ class ReservationController extends Controller
 {
     public function list($shop_id)
     {
-        $now = Carbon::now();
-
         $page = request()->query('page', 'reservation');
 
         $shop = Shop::with(['reservations.user'])->find($shop_id);
 
         if ($page === 'reservation') {
-            $reservations = $shop->reservations()->whereRaw("CONCAT(date, ' ', time) > ?", [$now])->get();
+            $reservations = $shop->reservations()->where('checked_in', false)->get();
         } elseif ($page === 'visited') {
-            $reservations = $shop->reservations()->whereRaw("CONCAT(date, ' ', time) <= ?", [$now])->get();
+            $reservations = $shop->reservations()->where('checked_in', true)->get();
         }
 
         return view('owner.reservation', compact('shop', 'reservations', 'page'));
